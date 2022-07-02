9df5875d5bebefcd2a90b161abb34e4e38c8f9dd
Ryan Low <ryan_low@rp.edu.sg>
2022-07-02 14:00:09 +0800

    commit message goes here

---
+++ web/private/action_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $t = date("H");
+  if ($t < "20") {
+  ?>
+    echo "Have a good day!";
+  <?php
+  } else {
+    echo "Have a good night!";
+  }
+  ?>
+  Welcome <span id="username"><?php echo $_POST["uname"]; ?></span><br>
+  Welcome <span id="password"><?php echo $_POST["psw"]; ?></span><br>
+
+</body>
+
+</html>
