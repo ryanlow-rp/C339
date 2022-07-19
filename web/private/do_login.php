0e6906210d6fd817e4d26aeec59b6b7a052dfdce
Ryan Low <ryan_low@rp.edu.sg>
2022-07-19 14:44:28 +0800

    commit message goes here

---
+++ web/private/do_login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  if ($_POST["password"] == "defaultpassword") {
+  ?>
+    Login for <span id="message"><?php echo $_POST["username"]; ?></span> succeeded! <br>
+  <?php
+  } else {
+  ?>
+    <span id="message">Login was not successful</span>
+  <?php
+  }
+  ?>
+</body>
+
+</html>
